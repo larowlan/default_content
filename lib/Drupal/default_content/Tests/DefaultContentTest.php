421b5a84752a848fdcba4ffc1817334f25494a0c
Lee Rowlands <lee.rowlands@previousnext.com.au>
2013-11-14 17:20:50 +1000

    Remove dependency on yaml_serialization

--- lib/Drupal/default_content/Tests/DefaultContentTest.php
+++ lib/Drupal/default_content/Tests/DefaultContentTest.php
@@ -15,7 +15,7 @@ class DefaultContentTest extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = array('rest', 'taxonomy', 'hal', 'yaml_serialization', 'default_content');
+  public static $modules = array('rest', 'taxonomy', 'hal', 'default_content');
 
   public static function getInfo() {
     return array(
