93824c2665468921c2fad93e70c5618979c4afaa
heykarthikwithu <heykarthikwithu@2833651.no-reply.drupal.org>
2016-03-21 22:43:38 +0200

    Issue #2691253 by heykarthikwithu: Unused imports and unnessary fully qualified names in code base

--- src/DefaultContentServiceProvider.php
+++ src/DefaultContentServiceProvider.php
@@ -7,9 +7,18 @@
 
 namespace Drupal\default_content;
 
-class DefaultContentServiceProvider extends \Drupal\Core\DependencyInjection\ServiceProviderBase {
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
 
-  public function alter(\Drupal\Core\DependencyInjection\ContainerBuilder $container) {
+/**
+ * A default content service provider.
+ */
+class DefaultContentServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
     $container
       ->getDefinition('rest.link_manager.type')
       ->setClass('Drupal\default_content\LinkManager\TypeLinkManager');
