9d068fac281e5d1e1249eeee8d89a117bdefaf50
alexpott <alexpott@157725.no-reply.drupal.org>
2017-04-24 04:19:11 +0300

    Issue #2833562 by alexpott, das-peter, tstoeckler, andypost, larowlan, hatznie: Installing modules using the ConfigImporter does not work

--- tests/src/Functional/DefaultContentTest.php
+++ tests/src/Functional/DefaultContentTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\default_content\Functional;
 
+use Drupal\Core\Config\FileStorage;
 use Drupal\simpletest\BrowserTestBase;
 use Drupal\simpletest\ContentTypeCreationTrait;
 use Drupal\simpletest\NodeCreationTrait;
@@ -35,11 +36,44 @@ class DefaultContentTest extends BrowserTestBase {
    * Test importing default content.
    */
   public function testImport() {
-    // Login as admin.
-    $this->drupalLogin($this->drupalCreateUser(array_keys(\Drupal::moduleHandler()->invokeAll(('permission')))));
     // Enable the module and import the content.
     \Drupal::service('module_installer')->install(array('default_content_test'), TRUE);
     $this->rebuildContainer();
+
+    $this->doPostInstallTests();
+  }
+
+  /**
+   * Test importing default content via ConfigImporter.
+   */
+  public function testImportViaConfigImporter() {
+    $sync = $this->container->get('config.storage.sync');
+    $this->copyConfig($this->container->get('config.storage'), $sync);
+
+    // Enable the module using the ConfigImporter.
+    $extensions = $sync->read('core.extension');
+    $extensions['module']['default_content_test'] = 0;
+    $extensions['module'] = module_config_sort($extensions['module']);
+    $sync->write('core.extension', $extensions);
+    // Slightly hacky but we need the config from the test module too.
+    $module_storage = new FileStorage(drupal_get_path('module', 'default_content_test') . '/config/install');
+    foreach ($module_storage->listAll() as $name) {
+      $sync->write($name, $module_storage->read($name));
+    }
+    $this->configImporter()->import();
+
+    $this->doPostInstallTests();
+  }
+
+  /**
+   * Makes assertions post the install of the default_content_test module.
+   */
+  protected function doPostInstallTests() {
+    // Login as admin.
+    $this->drupalLogin($this->drupalCreateUser([], NULL, TRUE));
+
+    // Ensure the content contained in the default_content_test module has been
+    // created correctly.
     $node = $this->getNodeByTitle('Imported node');
     $this->assertEquals($node->body->value, 'Crikey it works!');
     $this->assertEquals($node->getType(), 'page');
